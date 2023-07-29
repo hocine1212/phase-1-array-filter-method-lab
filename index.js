03b61e503dd54719ba7e619cbf4f97663489d8c5
Hocine Benouddane <hocine20992@gmail.com>
2023-07-29 21:43:31 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(arr, name) {
+  const driver = arr.filter((e) => e.toUpperCase() === name.toUpperCase());
+  return driver;
+}
+
+function fuzzyMatch(arr, name) {
+  const driver = arr.filter((e) => e.slice(0, 2) === name.slice(0, 2));
+  return driver;
+}
+
+function matchName(arr, name) {
+  const driver = arr.filter((e) => e.name === name);
+  return driver;
+}
